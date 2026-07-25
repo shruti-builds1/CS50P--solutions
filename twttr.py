4b932a9c6d0b92b073ec80f3f6018d37ae3acf3c
Shruti Singh <shruti.builds1@gmail.com>
2026-07-25 22:11:33 +0530

    Update twttr.py

--- twttr.py
+++ twttr.py
@@ -1,9 +1,23 @@
-user=input('Input: ')
-l=[]
-for k in user :
-    if k.lower() not in 'aeiou' :
-        l.append(k)
-print('Output: \n')
-for i in l :
-    print(i,end='')
-print()
+# user=input('Input: ')
+# l=[]
+# for k in user :
+#     if k.lower() not in 'aeiou' :
+#         l.append(k)
+# print('Output: \n')
+# for i in l :
+#     print(i,end='')
+# print()
+
+def main():
+    user=input('Input: ')
+    print('Output:',shorten(user))
+
+def shorten(word):
+    s=''
+    for k in word :
+        if k.lower() not in 'aeiou' :
+            s+=k
+    return s
+
+if __name__ == "__main__":
+    main()
